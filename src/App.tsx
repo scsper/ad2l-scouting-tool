bf86eba4fa363149b4a598859d2ad90642a404ec
Scott Sperling <scsper@gmail.com>
2025-12-03 12:01:02 -0600

    Remove unneeded boilerplate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
-
 export const App = () => (
   <div className="App">
     <input type="text" placeholder="Enter the league id" />
