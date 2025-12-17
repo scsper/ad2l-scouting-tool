2b5b7be593eb05a62f6ec0c7e1bde785161d4979
Scott Sperling <scsper@gmail.com>
2025-12-17 17:13:46 -0600

    Mini refactor for cleaner code

--- src/features/matches/aggregate-heroes-played-by-position.tsx
+++ src/features/matches/aggregate-heroes-played-by-position.tsx
@@ -20,29 +20,26 @@ export const AggregateHeroesPlayedByPosition = ({leagueId, teamId}: {leagueId: n
     <div className="px-4">
       <h2 className="text-xl font-bold">Aggregated Heroes Played By Position</h2>
       <div>
-        <h3 className="text-lg font-bold">Carry</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_1} />
-        <h3 className="text-lg font-bold">Mid</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_2} />
-        <h3 className="text-lg font-bold">Offlane</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_3} />
-        <h3 className="text-lg font-bold">Soft Support</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_4} />
-        <h3 className="text-lg font-bold">Hard Support</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_5} />
-        <h3 className="text-lg font-bold">Uncategorized</h3>
-        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.UNCATEGORIZED} />
+        <HeroList position="Carry" heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_1} />
+        <HeroList position="Mid" heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_2} />
+        <HeroList position="Offlane" heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_3} />
+        <HeroList position="Soft Support" heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_4} />
+        <HeroList position="Hard Support" heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_5} />
+        <HeroList position="Uncategorized" heroes={matchesData.aggregate.heroesPlayedByPosition.UNCATEGORIZED} />
       </div>
     </div>
   )
 }
 
-const HeroList = ({heroes}: {heroes: Record<string, number>}) => {
+const HeroList = ({heroes, position}: {heroes: Record<string, number>; position: string}) => {
   return (
-    <ul>
-      {Object.entries(heroes).sort((a, b) => b[1] - a[1]).map(([heroId, count]) => (
-        <li key={heroId}>{getHero(heroId)} ({count})</li>
-      ))}
-    </ul>
+    <div>
+      <h4 className="text-lg font-bold">{position}</h4>
+      <ul>
+        {Object.entries(heroes).sort((a, b) => b[1] - a[1]).map(([heroId, count]) => (
+          <li key={heroId}>{getHero(heroId)} ({count})</li>
+        ))}
+      </ul>
+    </div>
   )
 }
