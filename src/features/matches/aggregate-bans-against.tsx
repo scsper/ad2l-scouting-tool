d61a7d524be4f12022d218ccde8c178ccb56b74c
Scott Sperling <scsper@gmail.com>
2025-12-17 17:10:57 -0600

    Add aggregate heroes played by position

--- src/features/matches/aggregate-bans-against.tsx
+++ src/features/matches/aggregate-bans-against.tsx
@@ -4,20 +4,26 @@ import { getHero } from "../../utils/get-hero";
 export const AggregateBansAgainst = ({leagueId, teamId}: {leagueId: number; teamId: number}) => {
   const { data: matchesData, isLoading: isLoadingMatches, isError: isErrorMatches } = useGetMatchesQuery({ leagueId, teamId })
 
-  if (isLoadingMatches || isErrorMatches) {
+  if (isLoadingMatches) {
     return <div>Loading...</div>
+  }
+
+  if (isErrorMatches) {
+    return <div>Error: Please try again</div>
+  }
+
+  if (!matchesData) {
+    return <div>No matches found</div>
   }
 
   return (
     <div className="px-4">
       <h2 className="text-xl font-bold">Aggregated Bans Against</h2>
       <ul>
-        {Object.entries(matchesData?.aggregate.bansAgainst ?? {}).sort((a, b) => b[1] - a[1]).map(([heroId, count]) => (
+        {Object.entries(matchesData.aggregate.bansAgainst).sort((a, b) => b[1] - a[1]).map(([heroId, count]) => (
           <li key={heroId}>{getHero(heroId)} ({count})</li>
         ))}
       </ul>
     </div>
   )
-
-  return <div className="px-4">AggregateBansAgainst {leagueId} {teamId}</div>
 }
--- src/features/matches/aggregate-heroes-played-by-position.tsx
+++ src/features/matches/aggregate-heroes-played-by-position.tsx
@@ -1,3 +1,48 @@
+import { useGetMatchesQuery } from "./matches-api";
+import { getHero } from "../../utils/get-hero";
+
 export const AggregateHeroesPlayedByPosition = ({leagueId, teamId}: {leagueId: number; teamId: number}) => {
-  return <div className="px-4">AggregateHeroesPlayedByPosition {leagueId} {teamId}</div>
+  const { data: matchesData, isLoading: isLoadingMatches, isError: isErrorMatches } = useGetMatchesQuery({ leagueId, teamId })
+
+  if (isLoadingMatches) {
+    return <div>Loading...</div>
+  }
+
+  if (isErrorMatches) {
+    return <div>Error: Please try again</div>
+  }
+
+  if (!matchesData) {
+    return <div>No matches found</div>
+  }
+
+  return (
+    <div className="px-4">
+      <h2 className="text-xl font-bold">Aggregated Heroes Played By Position</h2>
+      <div>
+        <h3 className="text-lg font-bold">Carry</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_1} />
+        <h3 className="text-lg font-bold">Mid</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_2} />
+        <h3 className="text-lg font-bold">Offlane</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_3} />
+        <h3 className="text-lg font-bold">Soft Support</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_4} />
+        <h3 className="text-lg font-bold">Hard Support</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.POSITION_5} />
+        <h3 className="text-lg font-bold">Uncategorized</h3>
+        <HeroList heroes={matchesData.aggregate.heroesPlayedByPosition.UNCATEGORIZED} />
+      </div>
+    </div>
+  )
+}
+
+const HeroList = ({heroes}: {heroes: Record<string, number>}) => {
+  return (
+    <ul>
+      {Object.entries(heroes).sort((a, b) => b[1] - a[1]).map(([heroId, count]) => (
+        <li key={heroId}>{getHero(heroId)} ({count})</li>
+      ))}
+    </ul>
+  )
 }
